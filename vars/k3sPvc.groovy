efdad4457e14eb394693501fb935a026bca31f04
Leonardo <leonardo.rocha1990@gmail.com>
2021-11-23 16:26:40 -0600

    Updating pvc command

--- vars/k3sPvc.groovy
+++ vars/k3sPvc.groovy
@@ -1,7 +1,6 @@
 def call(Map config) { 
     String kpvc = sh(returnStdout: true, script: 'kubectl get pvc')
-    if(kpvc.count(config.name) > 0){
-        sh "kubectl delete pvc ${config.name}"
+    if(kpvc.count(config.name) == 0){
+        sh "kubectl create -f k3s/pvc.yml"
     }
-    sh "kubectl create -f k3s/pvc.yml"
 }
