49ff0a2a4116b0ae5a8a258bbf4f2da30e758b1f
roachmaster <leonardo.rocha1990@gmail.com>
2021-11-27 21:21:42 -0600

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,5 +2,5 @@ def call(Map config) {
     sh "docker build ${config.dockerOpt} -t ${config.dockerName} ."
     sh "docker login --username ${config.DOCKER_USERNAME} --password ${config.DOCKER_PASSWORD}"
     sh "docker push ${config.dockerName}"
-    sh "docker rmi ${config.dockerName}"
+    //sh "docker rmi ${config.dockerName}"
 }
