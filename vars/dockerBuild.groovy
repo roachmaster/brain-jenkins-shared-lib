c9bc89aa118ce0a554a8d4f360c605fc82543dfc
Leonardo <leonardo.rocha1990@gmail.com>
2022-01-19 15:16:48 -0600

    adding host to coker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(Map config) {
-    sh "docker build ${config.dockerOpt} -t ${config.dockerName} ."
+    sh "docker build ${config.dockerOpt} -t ${config.dockerName} . --network=host"
     sh "docker login --username ${config.DOCKER_USERNAME} --password ${config.DOCKER_PASSWORD}"
     sh "docker push ${config.dockerName}"
     sh "docker rmi ${config.dockerName}"
